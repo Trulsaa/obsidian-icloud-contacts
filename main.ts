2f667ec2e1bf57cfaea199787f92e3a841157e18
Truls Aagaard <Truls.Aagaard@skatteetaten.no>
2024-02-19 22:03:02 +0100

    BUGFIX iCloudVCard was not added.
    In the new version of createContactFile, the iCloudVCard was not added.
    This caused an error on later updates.

--- main.ts
+++ main.ts
@@ -273,6 +273,7 @@ export default class ICloudContacts extends Plugin {
 			for (const [key, value] of Object.entries(frontMatter)) {
 				fm[key] = value;
 			}
+			fm[iCloudVCardPropertieName] = iCloudVCard
 		});
 	}
 
